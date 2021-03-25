357722e7ff8e7e68c8ecdb03bec6099e2ef128b3
Redknignt1109 <72767525+Redknignt1109@users.noreply.github.com>
2021-03-25 17:17:38 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,12 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showString("" + (input.temperature()))
+})
+input.onButtonPressed(Button.AB, function () {
+	
+})
+input.onButtonPressed(Button.B, function () {
+	
+})
+basic.forever(function () {
+	
+})
